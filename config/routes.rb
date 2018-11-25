142839b49b730af6a2f55f2ea5c8ed07e42b77ff
hamityay <hamityay@hotmail.com>
2018-11-25 22:48:01 +0300

    Generated user and updated devise views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_path_names = {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'secret',
+      confirmation: 'verification'
+  }
+  # Users
+  devise_for :users,
+             path_names: devise_path_names
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 end

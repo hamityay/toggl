3408dc4c5d0461754adf8492b456e3032877c4e8
hamityay <hamityay@hotmail.com>
2018-11-28 04:26:06 +0300

    Added callback to timer.rb to fill duration field after_create.

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,4 +1,12 @@
 class Timer < ApplicationRecord
   belongs_to :type
   belongs_to :user
+
+  after_create :set_duration
+
+  private
+
+  def set_duration
+    self.update(duration: (finish - start) / 60.0)
+  end
 end
